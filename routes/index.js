0f93ef9b0112eb12efa2af6c1f533faa80d92036
hhvyas <hardikvyas1999@gmail.com>
2021-09-03 11:00:10 +0530

    DB wise chart added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const Award_Form = require('../models/form');
+const Final_Award_Form = require('../models/final_form');
 const router = express.Router();
 var ObjectID = require('mongodb').ObjectID;
 
@@ -182,6 +183,56 @@ router.get("/events", async (req, res) => {
 router.put('/events/:id', async(req, res) => {
     // console.log(req.body);
     console.log("PUT Called");
+    
+    console.log(req.body['Status']);
+    if (req.body['Status'] === 1){
+        console.log(req.params.id);
+        let ress = await Award_Form.find({_id: req.params.id});
+        if (ress){
+
+            console.log(ress[0]);
+
+            let received_data =ress[0];
+            console.log(received_data['Enrollment_Number'])
+            const Enrollment_Number= received_data['Enrollment_Number'];
+            const First_Name= received_data['First_Name'];
+            const Last_Name= received_data['Last_Name'];
+            const Email_id= received_data['Email_id'];
+            const Gender= received_data['Gender'];
+            const School_Name= received_data['School_Name'];
+            const Course_Name= received_data['Course_Name'];
+            const Semester= received_data['Semester'];
+            const Event_Name= received_data['Event_Name'];
+            const Event_Type= received_data['Event_Type'];
+            const Event_Level= received_data['Event_Level'];
+            const Rank= received_data['Rank'];
+            const Event_Date= received_data['Event_Date'];
+            const datetime = received_data['datetime'];
+            var newData = await Final_Award_Form.create({
+                Enrollment_Number,
+                First_Name,
+                Last_Name,
+                Email_id,
+                Gender,
+                School_Name,
+                Course_Name,
+                Semester,
+                Event_Name,
+                Event_Type,
+                Event_Level,
+                Rank,
+                Event_Date,
+                datetime
+            });
+            let task = await Final_Award_Form.find({ "_id": newData._id });
+            if (task){
+                console.log("Event Added! to The Main DB");
+                res.send("Event Successfully Added!");
+            }else{
+                console.log("Event not Added! Broooo!!!!!!!! in MAIN DB");
+            }
+            }
+        }else{
     let id = req.params.id;
     const { Enrollment_Number,
         First_Name,
@@ -240,6 +291,7 @@ router.put('/events/:id', async(req, res) => {
             console.log(err);
         }
     }
+}
 });
 
 
@@ -250,17 +302,18 @@ router.get('/events/:id', async(req, res) => {
         res.send(events[0]);
     });
 
-router.delete('/events/:id/show', async(req, res) => {
-    //console.log("Aavi gayo!")
+router.delete('/events/:id', async(req, res) => {
     const id = req.params.id;
     Award_Form.deleteOne({ _id: id }, (e) => {
-        //console.log(e);
+        
     });
     });
+
 
 
 router.get("/stats", async (req, res) => {
     const { sort, filter, range } = req.query;
+    console.log(req.query['q'])
     
     var stats = {
         gender: {},
@@ -269,6 +322,33 @@ router.get("/stats", async (req, res) => {
         semester:{}
     };
     //let Score = await Award_Form.find({'Gender': 'Male'}).count();
+    if (req.query['q'] === "Final"){
+    var events = await Final_Award_Form.find();
+
+    for (let i=0;i<events.length;i++){
+        const event = events[i];
+        if (event.Gender in stats.gender){
+            stats.gender[event.Gender] += 1;
+        }else{
+            stats.gender[event.Gender] = 1;
+        }
+        if (event.School_Name in stats.school){
+            stats.school[event.School_Name] += 1;
+        }else{
+            stats.school[event.School_Name] = 1;
+        }
+        if (event.Semester in stats.semester){
+            stats.semester[event.Semester] += 1;
+        }else{
+            stats.semester[event.Semester] = 1;
+        }
+        if (event.Rank in stats.rank){
+            stats.rank[event.Rank] += 1;
+        }else{
+            stats.rank[event.Rank] = 1;
+        }
+    }
+}else{
     var events = await Award_Form.find();
 
     for (let i=0;i<events.length;i++){
@@ -294,6 +374,7 @@ router.get("/stats", async (req, res) => {
             stats.rank[event.Rank] = 1;
         }
     }
+}
 
     stats = JSON.parse(JSON.stringify(stats).split('"_id":').join('"id":'));
     console.log(stats);
